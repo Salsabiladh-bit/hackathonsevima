493f8db042de6d7b0e72b9d3b9ecde0211aaec22
Salsabiladh-bit <71073644+Salsabiladh-bit@users.noreply.github.com>
2020-09-11 22:24:53 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Penyebaran Covid-19</title>
+  </head>
+  <body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container text-center">
+    <h1 class="display-4">Covid-19</h1>
+    <p class="lead">
+        <h2>
+            DATA PENYEBARAN VIRUS COVID-19 DI DUNIA
+            <br> Mari bersama menjaga kesehatan diri kita
+        </h2>
+    </p>
+  </div>
+</div>
+
+<style type="text/css">
+.box{
+    padding: 30px 40px;
+    border-radius: 5px;;
+}
+
+</style>
+<div class="container">
+    <div class"row">
+        <div class"col-md-4">
+            <div class="bg-danger box">
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    
+  </body>
+</html>
